25364cc493ca4e609cd930b78b4744c0a8dc3304
adastr96 <adam19960@op.pl>
2019-11-05 00:57:25 +0100

    header created

---
+++ src/views/Root/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import './App.css';
+
+class App extends React.Component {
+
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
